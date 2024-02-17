c5dee35a04bcb086d37bcc74a839f8ad40c22ad1
k-boychenko <manekinekoster@gmail.com>
2024-02-17 23:13:06 +0900

    Add prayer timetable

--- src/Pages/PrayerTimes.tsx
+++ src/Pages/PrayerTimes.tsx
@@ -1,24 +1,108 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 // components
 import { HeaderContext, HeaderContextType } from "../styles/HeaderContext";
 import { PAGE_NAMES } from "../common/Const";
 
 function PrayerTimes() {
+  const today = new Date();
+
   // useContext
   const { updateActiveTab } = useContext(HeaderContext) as HeaderContextType;
+
+  // useState
+  const [prayerTimesData, setPrayerTimesData] = useState([]);
+  const [dateDisplayed, setDateDisplayed] = useState(today);
 
   // useEffect
   useEffect(() => {
     updateActiveTab(PAGE_NAMES.prayerTimes);
+
+    fetchData(dateDisplayed.getFullYear(), dateDisplayed.getMonth() + 1);
   }, []);
+
+  // TODO: params: month, year
+  const fetchData = async (year: number, month: number) => {
+    const data = await fetch(
+      `http://api.aladhan.com/v1/calendarByCity/${year}/${month}?city=Yokohama&country=Japan&method=3`
+    );
+    const dataRes = await data.json();
+
+    setPrayerTimesData(dataRes.data);
+  };
+
+  const handlePrevMonthOnClick = () => {
+    dateDisplayed.setMonth(dateDisplayed.getMonth() - 1);
+    setDateDisplayed(dateDisplayed);
+    fetchData(dateDisplayed.getFullYear(), dateDisplayed.getMonth() + 1);
+  };
+
+  const handleNextMonthOnClick = () => {
+    dateDisplayed.setMonth(dateDisplayed.getMonth() + 1);
+    setDateDisplayed(dateDisplayed);
+    fetchData(dateDisplayed.getFullYear(), dateDisplayed.getMonth() + 1);
+  };
 
   return (
     <div className="page-container">
-      <h2>Prayer times</h2>
-      <div>
-        Here will be a list of monthly prayer times, including a downloadable
-        pdf
+      <h2>Monthly prayer timetable</h2>
+      <div className="page-content-container">
+        <div>Location: Yokohama</div>
+        <div>Calculation method: Muslim World Leage</div>
+        <div className="disp-flex prayer-timetable-container">
+          <div className="disp-flex prayer-timetable-month-header">
+            <h3
+              className="hover-cursor prayer-timetable-month-arrow"
+              onClick={() => handlePrevMonthOnClick()}
+            >
+              &#60;
+            </h3>
+            <h3>
+              {dateDisplayed.toLocaleDateString("en-US", { month: "long" })}
+            </h3>
+            <h3
+              className="hover-cursor prayer-timetable-month-arrow"
+              onClick={() => handleNextMonthOnClick()}
+            >
+              &#62;
+            </h3>
+          </div>
+
+          <table className="prayer-times-table">
+            <thead>
+              <tr>
+                <th>Day</th>
+                <th>Fajr</th>
+                <th>Sunrise</th>
+                <th>Dhuhr</th>
+                <th>Asr</th>
+                <th>Maghrib</th>
+                <th>Isha</th>
+              </tr>
+            </thead>
+            <tbody>
+              {prayerTimesData.map((days: any) => (
+                <tr
+                  key={days.date.gregorian.day}
+                  className={
+                    today.getMonth() === dateDisplayed.getMonth() &&
+                    today.getDate() === Number(days.date.gregorian.day)
+                      ? "prayer-time-today"
+                      : ""
+                  }
+                >
+                  <td>{days.date.gregorian.day}</td>
+                  <td>{days.timings.Fajr.split(" ", 1)}</td>
+                  <td>{days.timings.Sunrise.split(" ", 1)}</td>
+                  <td>{days.timings.Dhuhr.split(" ", 1)}</td>
+                  <td>{days.timings.Asr.split(" ", 1)}</td>
+                  <td>{days.timings.Maghrib.split(" ", 1)}</td>
+                  <td>{days.timings.Isha.split(" ", 1)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
