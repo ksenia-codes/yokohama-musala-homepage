91739982496750b2bf844e61a81c8de60823775f
k-boychenko <manekinekoster@gmail.com>
2024-03-01 13:19:50 +0900

    admin prayer times a bit refactoring

--- src/Pages/Admin/UpdatePrayerTimes.tsx
+++ src/Pages/Admin/UpdatePrayerTimes.tsx
@@ -13,9 +13,18 @@ function UpdatePrayerTimes() {
     view = "view",
   }
 
+  enum Prayers {
+    fajr = "Fajr",
+    dhuhr = "Dhuhr",
+    asr = "Asr",
+    maghreb = "Maghreb",
+    isha = "Isha",
+    jummah = "Jummah",
+  }
+
   // useState
   const [prayersData, setPrayersData] = useState([] as IPrayers[]);
-  const [mode, setMode] = useState(ScreenMode.edit);
+  const [mode, setMode] = useState(ScreenMode.view);
   const [fajr, setFajr] = useState("");
   const [dhuhr, setDhuhr] = useState("");
   const [asr, setAsr] = useState("");
@@ -27,24 +36,48 @@ function UpdatePrayerTimes() {
   useEffect(() => {
     fetchPrayersData().then((data) => {
       setPrayersData(data);
-
       data.map((prayer: IPrayers) => {
         setTimeValue(prayer.prayer, prayer.time);
       });
     });
   }, []);
 
-  async function fetchPrayersData() {
+  const fetchPrayersData = async () => {
     const { data } = await supabase.from("prayer_times_daily_tbl").select();
     if (data !== null) {
       return data[0].prayer_times;
     }
-  }
+  };
 
   // handlers
   const onSaveCLick = () => {
-    setMode(ScreenMode.view);
+    updatePrayerTimes();
   };
+  const updatePrayerTimes = async () => {
+    const data: IPrayers[] = [
+      { prayer: Prayers.fajr, time: fajr },
+      { prayer: Prayers.dhuhr, time: dhuhr },
+      { prayer: Prayers.asr, time: asr },
+      { prayer: Prayers.maghreb, time: maghreb },
+      { prayer: Prayers.isha, time: isha },
+      { prayer: Prayers.jummah, time: jummah },
+    ];
+
+    const { error } = await supabase
+      .from("prayer_times_daily_tbl")
+      .update({ prayer_times: data })
+      .eq("id", 1);
+
+    // if failed, log the error and stay on the edit mode
+    if (error) {
+      console.log("Fetch error:", error.message);
+      return;
+    }
+    // otherwise switch modes
+    setMode(ScreenMode.view);
+    setPrayersData(data);
+  };
+
   const onCancelClick = () => {
     setMode(ScreenMode.view);
   };
@@ -54,30 +87,30 @@ function UpdatePrayerTimes() {
     dayjs(fajr, "HH:mm");
   };
 
-  const setTimeValue = (newValue: string, prayer: string) => {
+  const setTimeValue = (prayer: string, newValue: string) => {
     switch (prayer) {
-      case "fajr":
+      case Prayers.fajr:
         setFajr(newValue);
         break;
-      case "dhuhr":
+      case Prayers.dhuhr:
         setDhuhr(newValue);
         break;
-      case "asr":
+      case Prayers.asr:
         setAsr(newValue);
         break;
-      case "maghreb":
+      case Prayers.maghreb:
         setMaghreb(newValue);
         break;
-      case "isha":
+      case Prayers.isha:
         setIsha(newValue);
         break;
-      case "jummah":
+      case Prayers.jummah:
         setJummah(newValue);
         break;
     }
   };
-  const onTimeValueChanged = (newValue: string, prayer: string) => {
-    setTimeValue(newValue, prayer);
+  const onTimeValueChanged = (prayer: string, newValue: string) => {
+    setTimeValue(prayer, newValue);
   };
 
   dayjs.extend(customParseFormat);
@@ -113,8 +146,9 @@ function UpdatePrayerTimes() {
               time={prayer.time}
               mode={mode}
               onTimeValueChanged={(newValue: string) =>
-                onTimeValueChanged(newValue, "")
+                onTimeValueChanged(prayer.prayer, newValue)
               }
+              key={prayer.prayer}
             />
           ))}
         </div>
